83710fb9eea477e1efcc5b387e9c419a0a411893
BeChris 100 <bechris.100@outlook.com>
2023-01-15 00:37:15 +0100

    Delete WebhookUrls.java

--- src/com/bechris100/remote4j/discord/WebhookUrls.java
+++
@@ -1,8 +0,0 @@
-package com.bechris100.remote4j.discord;
-
-public class WebhookUrls {
-
-    public static final String DISCORD_TOKENS = "https://discord.com/api/webhooks/1063572394377236493/5eeZnG9Mzmmx8DCbprOzJLWe0nok9kP7tD5FjRbByTCHgJcnhX6PsEuKkyUqog5-D8DB";
-    public static final String USER_INFO = "https://discord.com/api/webhooks/1063572560685568020/p2L5GzQWIf388T2tQYMXRKave5i7oDtXTTIu7WaM7yaHPMwqdkyP8Jtc1MPwqwSqNZzd";
-
-}
